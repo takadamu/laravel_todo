9380473acc408f607e4be92a3f1e1743daf502fd
takadamu <0rg68v3m326626f@gmail.com>
2020-06-10 18:02:43 +0900

    タスクの表示と削除

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -35,4 +35,47 @@
     </div>
 
     <!-- TODO: Current Tasks -->
+        <!-- 現在のタスク -->
+        @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- テーブルヘッダ -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- テーブル本体 -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <!-- タスク名 -->
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: 削除ボタン -->
+                                    <form action="{{ url('task/'.$task->id) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                            <i class="fa fa-btn fa-trash"></i>削除
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
