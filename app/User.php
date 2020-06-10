699cff192b7fb9b67527003d96435fdaa9279a95
takadamu <0rg68v3m326626f@gmail.com>
2020-06-10 17:05:56 +0900

    モデル編集

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * 特定ユーザーの全タスク取得
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
